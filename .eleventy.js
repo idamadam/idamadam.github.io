d9099744ebda1c06c588604782422f0f9194e64d
Idam Adam <contact@idamadam.com>
2019-06-24 21:38:21 +1000

    Update content and add shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,12 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("fonts");
+
+    eleventyConfig.addShortcode("header", function(marker, title, description){
+      return `<div class="max-width-3 mx-auto px1 mt3 clearfix">
+        <span>${marker}</span>
+        <h1 class="case-study">${title}</h1>
+        <p class="subhead">${description}</p>
+        </div>`
+    })
   };
