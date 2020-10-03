c45c7a00930bf67f82de0a103263c1bad778b709
Idam Adam <contact@idamadam.com>
2020-10-03 12:11:37 +1000

    Separate blockquote to own component

--- content/foliocamp/index.jsx
+++ content/foliocamp/index.jsx
@@ -1,30 +1,12 @@
 import Markdown from "markdown-to-jsx";
 import { Global, css } from "@emotion/core";
 import Head from "next/head";
-import PropTypes from "prop-types";
 
 import Header from "../../shared/components/Header";
 import Title from "../../shared/components/Title";
 import Subheading from "../../shared/components/Subheading";
+import Blockquote from "../../shared/components/Blockquote";
 import essay from "./essay.md";
-
-const Blockquote = ({ children }) => (
-  <div
-    css={{
-      backgroundColor: "#FFF8F2",
-      padding: "1.5em",
-      margin: "1em 0",
-      borderRadius: "0.5em",
-      fontSize: "1.1em",
-    }}
-  >
-    {children}
-  </div>
-);
-
-Blockquote.propTypes = {
-  children: PropTypes.node,
-};
 
 const FolioCamp = () => (
   <div>
