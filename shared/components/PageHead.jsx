8b46e40eff2055673159bd7c73434fd94e65843a
Idam Adam <contact@idamadam.com>
2020-10-10 18:13:34 +1100

    Put global css in the page head

--- shared/components/PageHead.jsx
+++ shared/components/PageHead.jsx
@@ -1,6 +1,34 @@
 import Head from "next/head";
-import { GlobalCSS } from "@emotion/core";
 import PropTypes from "prop-types";
+import { Global, css } from "@emotion/core";
+
+const GlobalCSS = () => (
+  <Global
+    styles={css`
+      body {
+        font-family: ibm-plex-sans, -apple-system, BlinkMacSystemFont,
+          "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
+          "Segoe UI Emoji", "Segoe UI Symbol";
+        font-weight: 300;
+        font-size: 18px;
+        line-height: 1.5;
+        background: #fff1e5;
+        color: #232323;
+      }
+      h1,
+      h2 {
+        font-family: "source-serif-pro", -apple-system, BlinkMacSystemFont,
+          "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
+          "Segoe UI Emoji", "Segoe UI Symbol";
+      }
+      img {
+        width: 100%;
+        border-radius: 0.5em;
+        box-shadow: 0px 8px 16px #ffddcc;
+      }
+    `}
+  />
+);
 
 const PageHead = ({ title = "Idam Adam" }) => (
   <div>
