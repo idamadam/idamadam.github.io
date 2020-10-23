fae772f197960f76a978fbdf90a9ec4754c9ee30
Idam Adam <contact@idamadam.com>
2020-10-23 20:43:40 +1100

    Add ability to remove Box Shadow

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -3,6 +3,7 @@ interface ImageGridProps {
   gridTemplateColumns?: string;
   gridTemplateRows?: string;
   fullbleed?: boolean;
+  shadow?: boolean;
 }
 
 function ImageGrid({
@@ -10,6 +11,7 @@ function ImageGrid({
   gridTemplateColumns = "repeat(auto-fill, minmax(200px, 1fr))",
   gridTemplateRows = "1fr",
   fullbleed = false,
+  shadow = true,
 }: ImageGridProps) {
   const style = {
     display: "grid",
@@ -17,6 +19,10 @@ function ImageGrid({
     gridTemplateColumns,
     gridTemplateRows,
     gridColumn: fullbleed ? "1 / -1" : "2",
+    "& > img": {
+      // This is to only turn the shadow off, the shadow is defined in Image.tsx
+      boxShadow: shadow ? "" : "none",
+    },
   };
 
   return <div css={style}>{children}</div>;
