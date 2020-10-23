d23ba04ccb5cb6f9d6d189ffe7e10376c5c49deb
Idam Adam <contact@idamadam.com>
2020-10-23 16:28:26 +1100

    Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
-    "prettier/react",
-  ],
+  root: true,
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
   },
-  plugins: ["react", "prettier"],
+  plugins: ["react", "prettier", "@typescript-eslint"],
+  extends: [
+    "airbnb-typescript",
+    "prettier/react",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier/@typescript-eslint",
+  ],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/require-default-props": "off",
   },
+  ignorePatterns: [".eslintrc.js", "next.config.js"],
 };
