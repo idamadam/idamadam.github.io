c77b698894826ac8a8728307b24badf382a01b78
Idam Adam <contact@idamadam.com>
2020-10-23 16:30:20 +1100

    Convert older files to typescript

--- pages/track.tsx
+++ pages/track.tsx
@@ -6,7 +6,7 @@ function Track() {
   return (
     <body>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <link
