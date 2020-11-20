0d95a2819b321da7f6ec417dc4305a2f67efaae0
Idam Adam <contact@idamadam.com>
2020-11-20 22:25:59 +1100

    Use markdown in the intro

--- src/components/Homepage/Intro.tsx
+++ src/components/Homepage/Intro.tsx
@@ -1,25 +1,10 @@
-const Intro = () => (
-  <p>
-    Designing tools to help businesses succeed at{" "}
-    <a href="https://myob.com/" target="_blank" rel="noreferrer">
-      MYOB
-    </a>
-    .
-    <br />
-    Prior experience includes{" "}
-    <a href="https://canstar.com.au/" target="_blank" rel="noreferrer">
-      Canstar
-    </a>
-    ,{" "}
-    <a href="https://netengine.com.au/" target="_blank" rel="noreferrer">
-      NetEngine
-    </a>{" "}
-    &amp;{" "}
-    <a href="https://qut.edu.au/" target="_blank" rel="noreferrer">
-      QUT
-    </a>
-    .
-  </p>
-);
+import Markdown from "markdown-to-jsx";
+
+const IntroMarkdown = `
+  Designing tools to help businesses succeed at [MYOB](https://myob.com/).
+  Previously at [Canstar](https://canstar.com.au/), [NetEngine](https://netengine.com.au/) & [QUT](https://qut.edu.au/).
+`;
+
+const Intro = () => <Markdown>{IntroMarkdown}</Markdown>;
 
 export default Intro;
