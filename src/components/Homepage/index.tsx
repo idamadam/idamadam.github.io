1869eb43de8f8ebd81eb0e870b23726d88c497a8
Idam Adam <contact@idamadam.com>
2020-11-20 22:25:38 +1100

    Use Postlist from module

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,49 +1,16 @@
 /* eslint-disable @typescript-eslint/require-await */
 
-import Link from "next/link";
-
 import PageHead from "../PageHead";
 import Header from "../Header";
-import Hero from "../Post/Hero";
 import Intro from "./Intro";
+import PostList, { PostListProps } from "./PostList";
 
-const PostList = ({ posts }: HomeProps) => (
-  <div css={{ display: "grid" }}>
-    {posts.map((post) => (
-      <div
-        key={post.slug}
-        css={{
-          display: "grid",
-          gridTemplateColumns: "4fr 1fr",
-          alignItems: "center",
-          gridGap: "1em",
-        }}
-      >
-        <Hero images={post.metadata.heroImages} />
-        <Link href={`/content/${post.slug}`}>{post.metadata.title}</Link>
-      </div>
-    ))}
-  </div>
-);
-
-export interface HomeProps {
-  posts: Array<{
-    metadata: {
-      title: string;
-      heroImages: Array<{
-        image: string;
-        alt: string;
-      }>;
-    };
-    slug: string;
-  }>;
-}
-
-const Homepage = ({ posts }: HomeProps) => (
+const Homepage = ({ posts }: PostListProps) => (
   <div
     css={{
       display: "grid",
       gridTemplateColumns: "1fr min(65ch,100%) 1fr",
+      padding: "1em",
     }}
   >
     <PageHead />
