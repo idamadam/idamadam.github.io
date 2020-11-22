5f14e07adaa5e6662ee735bf10b553aed1a615c1
Idam Adam <contact@idamadam.com>
2020-11-22 19:04:09 +1100

    Remove Header elements

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,22 +1,19 @@
 /* eslint-disable @typescript-eslint/require-await */
 
 import PageHead from "../PageHead";
-import Header from "../Header";
 import Intro from "./Intro";
 import PostList, { PostListProps } from "./PostList";
 
 const Homepage = ({ posts }: PostListProps) => (
   <div
     css={{
+      padding: "1em",
       display: "grid",
       gridTemplateColumns: "1fr min(65ch,100%) 1fr",
-      padding: "1em",
     }}
   >
     <PageHead />
-    <Header />
     <div css={{ gridColumn: 2 }}>
-      <h1>Idam Adam</h1>
       <Intro />
       <PostList posts={posts} />
     </div>
