d93a4515b1ad95e31c7291d52b0df4f168102aac
Idam Adam <contact@idamadam.com>
2020-11-22 19:03:03 +1100

    Remove markdown & add stylign

--- src/components/Homepage/Intro.tsx
+++ src/components/Homepage/Intro.tsx
@@ -1,10 +1,38 @@
-import Markdown from "markdown-to-jsx";
-
-const IntroMarkdown = `
-  Designing tools to help businesses succeed at [MYOB](https://myob.com/).
-  Previously at [Canstar](https://canstar.com.au/), [NetEngine](https://netengine.com.au/) & [QUT](https://qut.edu.au/).
-`;
-
-const Intro = () => <Markdown>{IntroMarkdown}</Markdown>;
+const Intro = () => (
+  <div css={{ marginTop: "4em" }}>
+    <p
+      css={{
+        fontSize: "2em",
+        fontWeight: 700,
+        marginBottom: 0,
+        color: "#B5866E",
+      }}
+    >
+      idam adam
+    </p>
+    <p css={{ fontSize: "2em", marginTop: "0.5em", marginBottom: 0 }}>
+      Product Designer at{" "}
+      <a href="https://myob.com/" target="_blank" rel="noreferrer">
+        MYOB
+      </a>
+      .
+    </p>
+    <p>
+      Previously at{" "}
+      <a href="https://canstar.com.au/" target="_blank" rel="noreferrer">
+        Canstar
+      </a>
+      ,{" "}
+      <a href="https://netengine.com.au/" target="_blank" rel="noreferrer">
+        NetEngine
+      </a>
+      ,{" "}
+      <a href="https://qut.edu.au/" target="_blank" rel="noreferrer">
+        QUT
+      </a>
+      .
+    </p>
+  </div>
+);
 
 export default Intro;
