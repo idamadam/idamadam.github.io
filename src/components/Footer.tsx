80acfa2264a2fe8f14e3f136c77056f9e70318fd
Idam Adam <contact@idamadam.com>
2020-11-25 13:14:03 +1100

    Include Techstack

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,24 @@
 const year = new Date().getFullYear();
 
 const Footer = () => (
-  <footer css={{ textAlign: "center", padding: "2em" }}>
-    © {year} idam adam
+  <footer css={{ textAlign: "center", padding: "2em", color: "#B5866E" }}>
+    <span css={{ fontWeight: 500 }}>© {year} idam adam</span>
+    <br />
+    <span css={{ fontSize: "0.8em" }}>
+      Built with{" "}
+      <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+        Next.js
+      </a>
+      . Read my code on{" "}
+      <a
+        href="https://github.com/idamadam/idamadam.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Github
+      </a>
+    </span>
   </footer>
 );
 
